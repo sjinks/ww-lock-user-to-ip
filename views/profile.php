3671f0e816309b95cf88e07bf81f9ddcc9a1514e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-07-18 05:59:04 +0300

    Bug fixes

--- views/profile.php
+++ views/profile.php
@@ -7,7 +7,7 @@
 			<th><label for="psb_ip_list"><?=esc_html__('Allowed addresses', 'lock-user')?></label></th>
 			<td>
 				<textarea cols="30" rows="7" name="psb_ip_list" id="psb_ip_list"><?=esc_html($ips);?></textarea>
-				<p class="description"><?=esc_html__("One address per line", 'lock-user')?></p>
+				<p class="description"><?=esc_html__('One address per line', 'lock-user')?></p>
 			</td>
 		</tr>
 	</tbody>
