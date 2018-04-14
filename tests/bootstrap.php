fe41318b4227d68c00f2aba78907e2ab59c0fb88
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-04-14 11:58:39 +0300

    CI

---
+++ tests/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+$_tests_dir = getenv('WP_TESTS_DIR');
+
+if (!$_tests_dir) {
+	$_tests_dir = rtrim(sys_get_temp_dir(), '/\\') . '/wordpress-tests-lib';
+}
+
+if (!file_exists($_tests_dir . '/includes/functions.php')) {
+	throw new Exception("Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh?");
+}
+
+if ('1' == getenv('WP_MULTISITE') && !defined('MULTISITE')) {
+	define('MULTISITE', true);
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin()
+{
+	if (file_exists(WP_PLUGIN_DIR . '/ww-lock-user-to-ip') && is_link(WP_PLUGIN_DIR . '/ww-lock-user-to-ip')) {
+		unlink(WP_PLUGIN_DIR . '/ww-lock-user-to-ip');
+	}
+
+	symlink(dirname(dirname(__FILE__)), WP_PLUGIN_DIR . '/ww-lock-user-to-ip');
+
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+	activate_plugin('ww-lock-user-to-ip/plugin.php', '', true);
+}
+
+tests_add_filter('muplugins_loaded', '_manually_load_plugin');
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
